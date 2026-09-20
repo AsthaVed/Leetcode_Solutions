06702b23c1d744ffd014adf174dc41d4a2efa677
Astha Ved <79042188+AsthaVed@users.noreply.github.com>
2026-09-20 16:28:27 +0530

    Refactor twoSum function and remove unused code
    Removed commented-out code and cleaned up the twoSum function.

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,8 +3,6 @@
  * @param {number} target
  * @return {number[]}
  */
- // nums = [1,2,3,4]
- // target = 5  //O(n)
  var twoSum = function(nums, target) {
     const map = {}
 
@@ -18,32 +16,3 @@
         map[nums[i]] = i
     }
 }
-
-
- //it takes 33 ms O(n²)
-// var twoSum = function(nums, target) {
-//     let twoSum = 0;
-//     for(let i=0; i<nums.length-1; i++){
-//         for(let j=i+1; j<nums.length; j++){
-//             twoSum = nums[i]+nums[j];
-//             if(twoSum === target){
-//                 return [i,j]
-//             }
-//         }
-//     }
-// };
-
-
-//using map function
-// var twoSum = function(nums, target) {
-//     const map = new Map()
-//     for(let i=0; i<nums.length; i++){
-//         const complement = target - nums[i];
-
-//         if(map.has(complement)){
-//             return [map.get(complement), i]
-//         }
-
-//         map.set(nums[i], i);
-//     }
-// };
