c2a5aae50f25862e9e4c7a7836403d35e0982750
Astha Ved <79042188+AsthaVed@users.noreply.github.com>
2026-09-20 16:15:13 +0530

    Time: 33 ms (30.81%), Space: 54.4 MB (69.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,27 +5,27 @@
  */
  // nums = [1,2,3,4]
  // target = 5
-// var twoSum = function(nums, target) {
-//     let twoSum = 0;
-//     for(let i=0; i<nums.length-1; i++){
-//         for(let j=i+1; j<nums.length; j++){
-//             twoSum = nums[i]+nums[j];
-//             if(twoSum === target){
-//                 return [i,j]
-//             }
-//         }
-//     }
-// };
-
 var twoSum = function(nums, target) {
-    const map = new Map()
-    for(let i=0; i<nums.length; i++){
-        const complement = target - nums[i];
-
-        if(map.has(complement)){
-            return [map.get(complement), i]
+    let twoSum = 0;
+    for(let i=0; i<nums.length-1; i++){
+        for(let j=i+1; j<nums.length; j++){
+            twoSum = nums[i]+nums[j];
+            if(twoSum === target){
+                return [i,j]
+            }
         }
-
-        map.set(nums[i], i);
     }
 };
+
+// var twoSum = function(nums, target) {
+//     const map = new Map()
+//     for(let i=0; i<nums.length; i++){
+//         const complement = target - nums[i];
+
+//         if(map.has(complement)){
+//             return [map.get(complement), i]
+//         }
+
+//         map.set(nums[i], i);
+//     }
+// };
