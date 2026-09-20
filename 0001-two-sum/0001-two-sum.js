8b411da2bde29738e97f5a8c4ca3b98464fd0e64
Astha Ved <79042188+AsthaVed@users.noreply.github.com>
2026-09-20 16:12:25 +0530

    Time: 2 ms (70.48%), Space: 55.4 MB (34.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,14 +5,27 @@
  */
  // nums = [1,2,3,4]
  // target = 5
+// var twoSum = function(nums, target) {
+//     let twoSum = 0;
+//     for(let i=0; i<nums.length-1; i++){
+//         for(let j=i+1; j<nums.length; j++){
+//             twoSum = nums[i]+nums[j];
+//             if(twoSum === target){
+//                 return [i,j]
+//             }
+//         }
+//     }
+// };
+
 var twoSum = function(nums, target) {
-    let twoSum = 0;
-    for(let i=0; i<nums.length-1; i++){
-        for(let j=i+1; j<nums.length; j++){
-            twoSum = nums[i]+nums[j];
-            if(twoSum === target){
-                return [i,j]
-            }
+    const map = new Map()
+    for(let i=0; i<nums.length; i++){
+        const complement = target - nums[i];
+
+        if(map.has(complement)){
+            return [map.get(complement), i]
         }
+
+        map.set(nums[i], i);
     }
 };
